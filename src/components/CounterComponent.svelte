c50362d14d047815de5610d135811b90b2996d2c
İnanç Dokurel <inanc.dokurel@globalmaksimum.com>
2020-08-26 02:09:04 +0300

    added Redux to the template

---
+++ src/components/CounterComponent.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+    export let count: number;
+    export let text: string;
+    const dispatch = createEventDispatcher();
+
+    function increment() {
+        dispatch("increment");
+    }
+    function decrement() {
+        dispatch("decrement");
+    }
+    function setText(value: string) {
+        dispatch("setText", { value });
+    }
+</script>
+
+<style>
+    h1 {
+        color: #3f83f8;
+        font-size: 1.875rem;
+        font-weight: 200;
+        margin: 0;
+    }
+
+    p {
+        color: #374151;
+        margin: 0;
+        margin-top: 1rem;
+    }
+</style>
+
+<br />
+<h1>Counter</h1>
+<br />
+<div>
+    <div>
+        <span>Count: {count}</span>
+        <button on:click={increment}>+1</button>
+        <button on:click={decrement}>-1</button>
+    </div>
+</div>
+
+<br />
+<h1>TextBox</h1>
+<br />
+<div>
+    <div>
+        <span>Text: {text}</span>
+        <input value={text} on:input={(event) => setText(event.target.value)} />
+    </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,45 +1,91 @@
 <script>
-	import ExampleComponent from "../components/ExampleComponent.svelte";
+    import CounterComponent from "./../components/CounterComponent.svelte";
+    import TitleComponent from "../components/TitleComponent.svelte";
+    import { createStore } from "redux";
+    import { bind } from "svelte3-redux";
+    const initialState = {
+        count: 0,
+        text: "hello",
+    };
+
+    const reducer = (state = initialState, action: any) => {
+        switch (action.type) {
+            case "increment":
+                return { ...state, count: state.count + 1 };
+            case "decrement":
+                return { ...state, count: state.count - 1 };
+            case "setText":
+                return { ...state, text: action.text };
+            default:
+                return state;
+        }
+    };
+
+    const store = createStore(reducer);
+    const state = bind(store);
+
+    function increment() {
+        state.dispatch({ type: "increment" });
+    }
+    function decrement() {
+        state.dispatch({ type: "decrement" });
+    }
+    function setText(event: CustomEvent) {
+        state.dispatch({
+            type: "setText",
+            text: event.detail.value,
+        });
+    }
 </script>
 
 <style>
-	.centerer {
-		flex: 1 1 0%;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+    .centerer {
+        flex: 1 1 0%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
 
-	.button {
-		margin-top: 2.5rem;
-		padding: 0.75rem;
-		border-radius: 0.5rem;
-		box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
-		transition-property: background-color, color, box-shadow;
-		transition-duration: 200ms;
-		transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-		text-decoration: none;
-	}
+    .button {
+        margin-top: 2.5rem;
+        padding: 0.75rem;
+        border-radius: 0.5rem;
+        box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+            0 2px 4px -1px rgba(0, 0, 0, 0.06);
+        transition-property: background-color, color, box-shadow;
+        transition-duration: 200ms;
+        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+        text-decoration: none;
+    }
 
-	.button:focus {
-		outline: 0;
-		box-shadow: 0 0 0 3px rgba(118, 169, 250, 0.45);
-	}
+    .button:focus {
+        outline: 0;
+        box-shadow: 0 0 0 3px rgba(118, 169, 250, 0.45);
+    }
 
-	.button-pink {
-		color: #99154B;
-		background-color: #FAD1E8;
-	}
+    .button-pink {
+        color: #99154b;
+        background-color: #fad1e8;
+    }
 
-	.button-pink:hover, .button-pink:focus {
-		background-color: #F8B4D9;
-	}
+    .button-pink:hover,
+    .button-pink:focus {
+        background-color: #f8b4d9;
+    }
 </style>
 
 <div class="centerer">
-	<ExampleComponent
-		title="🌐 Sapper with TypeScript and GraphQL project base"
-		paragraph="This is an example route and component to make sure everything's working." />
-	<a class="button button-pink" href="/graphql">Check out the GraphQL playground!</a>
+    <TitleComponent
+        title="🌐 Sapper with TypeScript and Redux project base"
+        paragraph="This is an example route and component to make sure
+        everything's working." />
+    <a class="button button-pink" href="/">Check out this button!</a>
+    <CounterComponent
+        count={$state.count}
+        text={$state.text}
+        on:setText={(value) => setText(value)}
+        on:increment={increment}
+        on:decrement={decrement} />
+
 </div>
